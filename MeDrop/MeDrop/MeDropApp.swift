d33139f0836423901963e525dca187803e30e789
Hamp <yongbeomkwak@gmail.com>
2023-07-03 11:16:07 +0900

    :tada: :: 프로젝트 생성

---
+++ MeDrop/MeDrop/MeDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeDropApp.swift
+//  MeDrop
+//
+//  Created by yongbeomkwak on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct MeDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
