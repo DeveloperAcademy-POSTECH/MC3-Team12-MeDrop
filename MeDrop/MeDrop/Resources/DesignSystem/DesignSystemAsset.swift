1e9d83a0b9c2bee00b0edb76fa216f610866e718
Hamp <yongbeomkwak@gmail.com>
2023-07-08 20:35:30 +0900

    :sparkles:: CardColor 추가

--- MeDrop/MeDrop/Resources/DesignSystem/DesignSystemAsset.swift
+++ MeDrop/MeDrop/Resources/DesignSystem/DesignSystemAsset.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
 
 public enum DesignSystemAsset {
-    
+    public enum CardColor {
+        static let black = Color("Black")
+        static let blue1 = Color("Blue1")
+        static let neonPink = Color("NeonPink")
+        static let lime = Color("Lime")
+        static let white = Color("White")
+    }
 }
--- MeDrop/MeDrop/Sources/ContentView.swift
+++ MeDrop/MeDrop/Sources/ContentView.swift
@@ -13,19 +13,6 @@ struct ContentView: View {
             Text("10")
         }
         .padding()
-        .onAppear{
-            FireBaseDataBaseManager.shared
-                .child("firest")
-                .observeSingleEvent(of: .value) { snapshot in
-                    print("SnapShot")
-                   
-                    guard let value = snapshot.value as? String else {
-                        print("RET")
-                        return
-                    }
-                    print(value)
-                }
-        }
     }
 }
 
