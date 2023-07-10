d33b2f99f08ffeeacd1989d2e2a1a22fbc89c7b4
Hamp <yongbeomkwak@gmail.com>
2023-07-10 12:01:42 +0900

    :sparkles:: 매니저 이동 및 구현
    1. 파베 매니저 이동
    2. 프리퍼런스 매니저 구현

---
+++ MeDrop/MeDrop/Sources/OnBoard/View/OnBoardView.swift
@@ -0,0 +1,23 @@
+//
+//  onBoardView.swift
+//  MeDrop
+//
+//  Created by yongbeomkwak on 2023/07/10.
+//
+
+import Foundation
+import SwiftUI
+
+struct OnBoardView : View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+struct OnBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardView()
+    }
+}
