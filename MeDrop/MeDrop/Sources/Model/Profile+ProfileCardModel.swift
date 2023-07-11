04000b9774459b01c51c0845663224fedbe2bb8f
abovocado <dkstjdud02@gmail.com>
2023-07-11 10:26:53 +0900

    ✨: DataModel에 link 필드 추가

--- MeDrop/MeDrop/Sources/Model/Profile+ProfileCardModel.swift
+++ MeDrop/MeDrop/Sources/Model/Profile+ProfileCardModel.swift
@@ -13,6 +13,7 @@ protocol ProfileCard: Identifiable {
     var contact: String { get set }
     var email: String { get set }
     var company: String { get set }
+    var link: String { get set}
     var job: String { get set }
     var introduction: String { get set }
     var colorSet: ColorSet { get set }
@@ -27,6 +28,7 @@ struct ProfileCardModel: ProfileCard {
     var contact: String = ""
     var email: String = ""
     var company: String = ""
+    var link: String = ""
     var job: String = ""
     var introduction: String = ""
     var colorSet: ColorSet = .white
@@ -50,10 +52,10 @@ struct ProfileCardModel: ProfileCard {
 
 extension ProfileCardModel {
     static let sampleData: [ProfileCardModel] = [
-        ProfileCardModel(name: "젤리빈", contact: "010-0000-0000", email: "jellyBeen@gmail.com", company: "􀣺 jelly Developer Academy", job: "Designer.", introduction: "한 줄 소개; 젤리빈 어쩌구 저쩌구", colorSet: .white),
-        ProfileCardModel(name: "릴리", contact: "010-1111-1111", email: "lily@gmail.com", company: "􀣺 lily Academy", job: "Developer.", introduction: "한 줄 소개; 릴리 개발자", colorSet: .neonPink),
-        ProfileCardModel(name: "호제", contact: "010-2222-2222", email: "hoje@gmail.com", company: "􀣺 hoje Developer ", job: "CEO.", introduction: "한 줄 소개; 호제 CEO", colorSet: .lime),
-        ProfileCardModel(name: "제이", contact: "010-3333-3333", email: "jay@gmail.com", company: "􀣺 jay Developer ", job: "CTO.", introduction: "한 줄 소개; 제이 CEO", colorSet: .black),
-        ProfileCardModel(name: "카일", contact: "010-4444-4444", email: "kayle@gmail.com", company: "􀣺 kayle Developer ", job: "COO.", introduction: "한 줄 소개; 카일 CEO", colorSet: .blue1)
+        ProfileCardModel(name: "젤리빈", contact: "010-0000-0000", email: "jellyBeen@gmail.com", company: "􀣺 jelly Developer Academy", link: "jellyBeen.com", job: "Designer.", introduction: "한 줄 소개; 젤리빈 어쩌구 저쩌구", colorSet: .white),
+        ProfileCardModel(name: "릴리", contact: "010-1111-1111", email: "lily@gmail.com", company: "􀣺 lily Academy", link: "lily.com", job: "Developer.", introduction: "한 줄 소개; 릴리 개발자", colorSet: .neonPink),
+        ProfileCardModel(name: "호제", contact: "010-2222-2222", email: "hoje@gmail.com", company: "􀣺 hoje Developer ", link: "hoje.com", job: "CEO.", introduction: "한 줄 소개; 호제 CEO", colorSet: .lime),
+        ProfileCardModel(name: "제이", contact: "010-3333-3333", email: "jay@gmail.com", company: "􀣺 jay Developer ", link: "jay.com", job: "CTO.", introduction: "한 줄 소개; 제이 CEO", colorSet: .black),
+        ProfileCardModel(name: "카일", contact: "010-4444-4444", email: "kayle@gmail.com", company: "􀣺 kayle Developer ", link: "kayle.com", job: "COO.", introduction: "한 줄 소개; 카일 CEO", colorSet: .blue1)
     ]
 }
