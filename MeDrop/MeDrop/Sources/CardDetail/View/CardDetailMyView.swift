ca0be9da7ce6f0a81ab17130c4621c8a8ff1cb10
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-07-27 14:41:07 +0900

    ✨: CardDetail 추가

---
+++ MeDrop/MeDrop/Sources/CardDetail/View/CardDetailMyView.swift
@@ -0,0 +1,34 @@
+//
+//  CardDetailMyView.swift
+//  MeDrop
+//
+//  Created by SY AN on 2023/07/24.
+//
+
+import SwiftUI
+
+struct CardDetailMyView: View {
+    @Binding var card: ProfileCardModel
+    @State var isShowingEdit: Bool = false
+    @State var editingCard = ProfileCardModel.emptyCard
+    var body: some View {
+        CardDetailView(card: $card, isFromMy: true)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("편집") {
+                        editingCard = card
+                        isShowingEdit.toggle()
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingEdit) {
+                EditCardView(originCard: $card, editingCard: $editingCard, isFinish: $isShowingEdit)
+            }
+    }
+}
+
+struct CardDetailMyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardDetailMyView(card: .constant(ProfileCardModel.sampleData[1]))
+    }
+}
---
+++ MeDrop/MeDrop/Sources/CardDetail/View/CardDetailView.swift
@@ -0,0 +1,69 @@
+//
+//  CardDetailView.swift
+//  MeDrop
+//
+//  Created by SY AN on 2023/07/23.
+//
+
+import SwiftUI
+
+struct CardDetailView: View {
+    @Binding var card: ProfileCardModel
+    var isFromMy: Bool
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                HStack {
+                    Text("\(card.introduction)").font(.title).bold()
+                    Spacer()
+                }
+                
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.white)
+                    .shadow(radius: 15)
+                    .overlay(
+                        VStack {
+                            Spacer()
+                            HStack {
+                                ForEach(ContactButton.allCases, id: \.self) { contactButton in
+                                    actionButtonDrawer(contactButton: contactButton, profileCard: card)
+                                        .disabled(isFromMy)
+                                }
+                            }
+                            .padding(.vertical)
+                            // 컴포넌트화 예정 HStack + 폰트 + Padding
+                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("\(card.name)").padding(.bottom)
+                                    Text("\(card.company)").padding(.bottom)
+                                    Text("\(card.job)").padding(.bottom)
+                                    Text("\(card.contact)").padding(.bottom)
+                                    Text("\(card.email)")
+                                }
+                                Spacer()
+                            }
+                            Spacer()
+                            HStack {
+                                ForEach(ContactButton.allCases, id: \.self) { contactButton in
+                                    actionButtonDrawer(contactButton: contactButton, profileCard: card)
+                                        .disabled(isFromMy)
+                                }
+                            }
+                            Spacer()
+                        }
+                            .padding()
+                    )
+            }
+            .padding()
+        }
+    }
+}
+
+struct CardDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardDetailView(card: .constant(ProfileCardModel.sampleData[1]), isFromMy: true)
+    }
+}
