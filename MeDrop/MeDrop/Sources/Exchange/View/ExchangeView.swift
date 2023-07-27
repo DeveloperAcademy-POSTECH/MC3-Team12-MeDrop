bea7b269189107d547ca46a0f3d55b9e62a44c4d
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-07-27 14:57:48 +0900

    ✨: ExchangeView 추가

---
+++ MeDrop/MeDrop/Sources/Exchange/View/ExchangeView.swift
@@ -0,0 +1,30 @@
+//
+//  ExchangeView.swift
+//  MeDrop
+//
+//  Created by SY AN on 2023/07/22.
+//
+
+import SwiftUI
+
+struct ExchangeView: View {
+    @Binding var collectedCards: [ProfileCardModel]
+    var newYourCard = ProfileCardModel.sampleData[1]
+    var body: some View {
+        VStack{
+            Text("Exchange View")
+            Button(action:{
+                collectedCards.append(newYourCard)
+                print(collectedCards)
+            }){
+                Text("교환")
+            }
+        }
+    }
+}
+
+struct ExchangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExchangeView(collectedCards: .constant(ProfileCardModel.sampleData))
+    }
+}
