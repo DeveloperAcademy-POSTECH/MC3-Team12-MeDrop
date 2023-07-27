220f083a53d888e3db4fc7835fdf70061be339f4
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-07-27 14:33:34 +0900

    ✨: My Cards View 추가

---
+++ MeDrop/MeDrop/Sources/MyCards/View/MyCards.swift
@@ -0,0 +1,79 @@
+//
+//  MyCardsView.swift
+//  MeDrop
+//
+//  Created by SY AN on 2023/07/22.
+//
+
+import SwiftUI
+
+struct MyCardsView: View {
+    @Binding var myCards: [ProfileCardModel]
+    @State private var newCard = ProfileCardModel.emptyCard
+    @State var selectedIndex = 0
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
+    @State var isMenu = false
+    @State var isDetail = false
+    @State var isCreate = false
+    
+    let saveAction: () -> Void
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TabView(selection: $selectedIndex) {
+                        ForEach($myCards.indices, id: \.self) { index in
+                            Button(action: {isDetail.toggle()}) {
+                                CardView(card: $myCards[index])
+                                    .frame(height: UIScreen.height * 0.65)
+                            }
+                            .padding()
+                            .tag(index)
+                            .navigationDestination(isPresented: $isDetail) {
+                                CardDetailMyView(card: $myCards[index])
+                            }
+                        }
+                        
+                        if $myCards.count == 5 {
+                            FinalCardView()
+                                .tag($myCards.count + 1)
+                                .padding()
+                                .frame(height: UIScreen.height * 0.65)
+                        } else {
+                            Button(action: {isCreate.toggle()}) {
+                                PlusCardView()
+                            }
+                            .frame(height: UIScreen.height * 0.65)
+                            .padding()
+                            .tag($myCards.count + 1)
+                        }
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            .navigationTitle("MY CARDS")
+            .toolbar {
+                Button(action: { isMenu.toggle() }) {
+                    Image(systemName: "ellipsis")
+                }
+                .navigationDestination(isPresented: $isMenu) {
+                    MenuView()
+                }
+            }
+            .sheet(isPresented: $isCreate) {
+                NewCardView(cards: $myCards, isFinish: $isCreate)
+            }
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
+    }
+}
+
+struct MyCardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyCardsView(myCards: .constant(ProfileCardModel.sampleData), saveAction: {})
+    }
+}
