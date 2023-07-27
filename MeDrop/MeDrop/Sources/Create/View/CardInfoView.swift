3e612ef6dcabc23d93e8712e0fed2a185cb3f4d5
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-07-27 15:31:14 +0900

    ✨: 한줄 소개 max 추가

--- MeDrop/MeDrop/Sources/Create/View/CardInfoView.swift
+++ MeDrop/MeDrop/Sources/Create/View/CardInfoView.swift
@@ -117,3 +117,14 @@ struct CardInfoView_Previews: PreviewProvider {
         CardInfoView (card: .constant(ProfileCardModel.emptyCard), isFinish: .constant(false), isShowingColorSelectView: .constant(false))
     }
 }
+
+extension Binding where Value == String {
+    func max(_ limit: Int) -> Self {
+        if self.wrappedValue.count >= limit {
+            DispatchQueue.main.async {
+                self.wrappedValue = String(self.wrappedValue.dropLast())
+            }
+        }
+        return self
+    }
+}
