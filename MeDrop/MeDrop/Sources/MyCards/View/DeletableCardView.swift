5856be977174a2498a7a58127e0dda18c95fe1bd
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-08-01 23:21:54 +0900

    🔥: dummy

---
+++ MeDrop/MeDrop/Sources/MyCards/View/DeletableCardView.swift
@@ -0,0 +1,20 @@
+//
+//  DeletableCardView.swift
+//  MeDrop
+//
+//  Created by SY AN on 2023/08/01.
+//
+
+import SwiftUI
+
+struct DeletableCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeletableCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeletableCardView()
+    }
+}
