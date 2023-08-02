e698a61768ab2972ca4a3ba25a1ca57755650272
Jose <dbswkgus99@snu.ac.kr>
2023-08-02 18:11:25 +0900

    🔥: 명함지갑 더미 데이터 삭제

--- MeDrop/MeDrop/Sources/Main/View/MainTabView.swift
+++ MeDrop/MeDrop/Sources/Main/View/MainTabView.swift
@@ -10,9 +10,6 @@ import SwiftUI
 struct MainTabView: View {
         @State var selectedTab: Tab = .my
         @State var showingAlert = false
-    @State var tempCards: [ProfileCardModel] = [ ProfileCardModel(name: "젤리빈", contact: "010-0000-0000", company: "􀣺 jelly Developer Academy", job: "Designer.", introduction: "한 줄 소개; 젤리빈 어쩌구 저쩌구", email: "jellyBeen@gmail.com", link: "jellyBeen.com", designType: "0-0", date: 20200102),
-                                                 ProfileCardModel(name: "릴리", contact: "010-1111-1111", company: "􀣺 lily Academy", job: "Developer.", introduction: "한 줄 소개; 릴리 개발자", email: "lily@gmail.com", link: "lily.com", insta: "@seooo_02", designType: "0-1", date: 20200103),
-                                                 ProfileCardModel(name: "호제", contact: "010-2222-2222", company: "􀣺 hoje Developer ", job: "CEO.", introduction: "한 줄 소개; 호제 CEO", email: "hoje@gmail.com", link: "hoje.com", designType: "1-2", date: 20200104)]
     
         @StateObject private var cardStore = CardStore()
         
@@ -28,9 +25,7 @@ struct MainTabView: View {
                                 }
                             }
                         case .your:
-                            CollectedCardsView(selectedTab: $selectedTab, yourCards:
-                                                $tempCards
-                                                //                                                $cardStore.yourCards
+                            CollectedCardsView(selectedTab: $selectedTab, yourCards: $cardStore.yourCards
                             ){
                                 Task {
                                     try await cardStore.saveData()
