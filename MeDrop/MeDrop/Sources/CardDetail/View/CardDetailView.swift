46180f848ec1ddccbbf945142df5f5d12dc7d606
SeoYoung.An <102544840+abovocado@users.noreply.github.com>
2023-08-02 15:54:38 +0900

    🎨: 애니메이션 버튼에서 제거

--- MeDrop/MeDrop/Sources/CardDetail/View/CardDetailView.swift
+++ MeDrop/MeDrop/Sources/CardDetail/View/CardDetailView.swift
@@ -108,7 +108,6 @@ struct CardDetailView: View {
                                 .shadow(color: Color(#colorLiteral(red: 0.051, green: 0.153, blue: 0.314, alpha: 1)).opacity(0.16), radius: 20, x: 28, y: 28)
                                 .shadow(color: Color.white.opacity(1), radius: 48, x: -23, y: -23)
                         }
-                           
                     }
                     .disabled(card.email.isEmpty)
                     
@@ -134,6 +133,7 @@ struct CardDetailView: View {
                                 .background(.white)
                                 .cornerRadius(50)
                                 .shadow(radius: 4)
+                                .animation(.spring())
                             }
                         }
                         Button(action: {
@@ -154,15 +154,13 @@ struct CardDetailView: View {
                             }
                         }
                     }
-                    .animation(.spring())
                 }
                 .padding()
             }
         }
         // 여기
         .background(
-            
-            VStack{
+            VStack {
                 Image("\(card.designType)-background")
                     .resizable()
                     .scaledToFit()
