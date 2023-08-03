a4bf2973d855452ee7f9efacbe36f333402f8697
Hamp <yongbeomkwak@gmail.com>
2023-08-03 09:27:59 +0900

    :art:: 생성 뷰 스크롤뷰 삽입

--- MeDrop/MeDrop/Sources/Create/View/EnterInfo1View.swift
+++ MeDrop/MeDrop/Sources/Create/View/EnterInfo1View.swift
@@ -17,134 +17,137 @@ struct EnterInfo1View: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                VStack(alignment: .leading) {
-                    Text("이름")
-                        .font(
-                        Font.custom("SF Pro Text", size: 17)
-                        .weight(.bold)
-                        )
-                    VStack {
-                        HStack {
-                            TextField("이름/닉네임 모두 가능해요.", text: $editingCard.name.max(8))
-                                .font(Font.custom("SF Pro Text", size: 17))
-
-                            Spacer()
-                            Text("\($editingCard.name.wrappedValue.count)/8").foregroundColor(.gray)
-                                .font(Font.custom("SF Pro Text", size: 13))
-                        }
-                        Rectangle()
-                            .frame(height: 1)
-                    } .foregroundColor(DesignSystemAsset.gray1)
-                }.padding()
-                VStack(alignment: .leading) {
-                    Text("소속").font(
-                        Font.custom("SF Pro Text", size: 17)
-                        .weight(.bold)
-                        )
-                    VStack {
-                        HStack {
-                            TextField("회사, 학교, 단체 등을 입력해주세요.", text: $editingCard.company)
-                                .font(Font.custom("SF Pro Text", size: 17))
-                            Spacer()
-                        }
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                    }.foregroundColor(DesignSystemAsset.gray1)
-                }.padding()
-                VStack(alignment: .leading) {
-                    Text("직무").font(
-                        Font.custom("SF Pro Text", size: 16)
-                        .weight(.bold)
-                        )
-                    VStack {
-                        HStack {
-                            TextField("", text: $editingCard.job.max(16))
-                                .font(Font.custom("SF Pro Text", size: 16))
-
-                            Spacer()
-                            Text("\($editingCard.job.wrappedValue.count)/16").foregroundColor(.gray)
-                                .font(Font.custom("SF Pro Text", size: 13))
-                        }
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                    }.foregroundColor(DesignSystemAsset.gray1)
-                }.padding()
-                
-                VStack(alignment: .leading) {
-                    Text("핸드폰 번호").font(
-                        Font.custom("SF Pro Text", size: 17)
-                        .weight(.bold)
-                        )
-                    VStack {
-                        HStack {
-                            
-                            NumberPhoneMaskView(text: $editingCard.contact.max(13)).keyboardType(.numberPad)
-                                .font(Font.custom("SF Pro Text", size: 17))
-                            Spacer()
-                        }
-                        Rectangle()
-                            .frame(height: 1)
-                            
-                    }.foregroundColor(DesignSystemAsset.gray1)
-                }.padding()
-                
-                Spacer()
-                
-                
-                HStack {
-                    
-                    Button(action: {}) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10).foregroundColor(DesignSystemAsset.white2)
-                            Text("이전")
-                                .font(Font.custom("SF Pro Text", size: 17))
-                                .foregroundColor(DesignSystemAsset.gray2)
-                        }
-                    }.disabled(true)
-                    
-                    Button(action:
-                            {
-                        withAnimation(.easeIn(duration: 0.2)){
-                            navigationControl += 1   
-                        }}) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(editingCard.complete() && editingCard.contact.count == 13 ? Color(red: 0.33, green: 0.38, blue: 0.54) : DesignSystemAsset.white2)
-                                Text("다음")
+            ScrollView{
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("이름")
+                            .font(
+                            Font.custom("SF Pro Text", size: 17)
+                            .weight(.bold)
+                            )
+                        VStack {
+                            HStack {
+                                TextField("이름/닉네임 모두 가능해요.", text: $editingCard.name.max(8))
                                     .font(Font.custom("SF Pro Text", size: 17))
-                                    .foregroundColor(DesignSystemAsset.white1)
+
+                                Spacer()
+                                Text("\($editingCard.name.wrappedValue.count)/8").foregroundColor(.gray)
+                                    .font(Font.custom("SF Pro Text", size: 13))
                             }
-                            }.disabled(editingCard.complete() && editingCard.contact.count == 13
-                                       ? false : true)
-                }
-                .frame(height: UIScreen.height * 0.07)
-                .padding()
-            }.navigationTitle("필수 정보를 입력해주세요.")
-            
-                .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
-                        Button(action: {
-                            isBack.toggle()
-                        }) {
-                            Image("x").resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: UIScreen.width*0.04, height: UIScreen.height * 0.04)
-                            .padding()
+                            Rectangle()
+                                .frame(height: 1)
+                        } .foregroundColor(DesignSystemAsset.gray1)
+                    }.padding()
+                    VStack(alignment: .leading) {
+                        Text("소속").font(
+                            Font.custom("SF Pro Text", size: 17)
+                            .weight(.bold)
+                            )
+                        VStack {
+                            HStack {
+                                TextField("회사, 학교, 단체 등을 입력해주세요.", text: $editingCard.company)
+                                    .font(Font.custom("SF Pro Text", size: 17))
+                                Spacer()
+                            }
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        }.foregroundColor(DesignSystemAsset.gray1)
+                    }.padding()
+                    VStack(alignment: .leading) {
+                        Text("직무").font(
+                            Font.custom("SF Pro Text", size: 16)
+                            .weight(.bold)
+                            )
+                        VStack {
+                            HStack {
+                                TextField("", text: $editingCard.job.max(16))
+                                    .font(Font.custom("SF Pro Text", size: 16))
+
+                                Spacer()
+                                Text("\($editingCard.job.wrappedValue.count)/16").foregroundColor(.gray)
+                                    .font(Font.custom("SF Pro Text", size: 13))
+                            }
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        }.foregroundColor(DesignSystemAsset.gray1)
+                    }.padding()
+                    
+                    VStack(alignment: .leading) {
+                        Text("핸드폰 번호").font(
+                            Font.custom("SF Pro Text", size: 17)
+                            .weight(.bold)
+                            )
+                        VStack {
+                            HStack {
+                                
+                                NumberPhoneMaskView(text: $editingCard.contact.max(13)).keyboardType(.numberPad)
+                                    .font(Font.custom("SF Pro Text", size: 17))
+                                Spacer()
+                            }
+                            Rectangle()
+                                .frame(height: 1)
+                                
+                        }.foregroundColor(DesignSystemAsset.gray1)
+                    }.padding()
+                    
+                    Spacer()
+                    
+                    
+                    HStack {
+                        
+                        Button(action: {}) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10).foregroundColor(DesignSystemAsset.white2)
+                                Text("이전")
+                                    .font(Font.custom("SF Pro Text", size: 17))
+                                    .foregroundColor(DesignSystemAsset.gray2)
+                            }
+                        }.disabled(true)
+                        
+                        Button(action:
+                                {
+                            withAnimation(.easeIn(duration: 0.2)){
+                                navigationControl += 1
+                            }}) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(editingCard.complete() && editingCard.contact.count == 13 ? Color(red: 0.33, green: 0.38, blue: 0.54) : DesignSystemAsset.white2)
+                                    Text("다음")
+                                        .font(Font.custom("SF Pro Text", size: 17))
+                                        .foregroundColor(DesignSystemAsset.white1)
+                                }
+                                }.disabled(editingCard.complete() && editingCard.contact.count == 13
+                                           ? false : true)
+                    }
+                    .frame(height: UIScreen.height * 0.07)
+                    .padding()
+                }.navigationTitle("필수 정보를 입력해주세요.")
+                
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button(action: {
+                                isBack.toggle()
+                            }) {
+                                Image("x").resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: UIScreen.width*0.04, height: UIScreen.height * 0.04)
+                                .padding()
+                            }
                         }
-                    }
-                }.alert("돌아가시겠어요?", isPresented: $isBack) {
-                    Button("확인", role: .destructive) {
-                        dismiss()
-                    }
-                    Button("취소", role: .cancel) {
-                        isBack.toggle()
-                    }
-                } message: {
-                    Text("지금 돌아간다면 정보는 저장되지 않습니다. ")
                 }
+            }
+            .alert("돌아가시겠어요?", isPresented: $isBack) {
+                Button("확인", role: .destructive) {
+                    dismiss()
+                }
+                Button("취소", role: .cancel) {
+                    isBack.toggle()
+                }
+            } message: {
+                Text("지금 돌아간다면 정보는 저장되지 않습니다. ")
+            }
         }
     }
 }
--- MeDrop/MeDrop/Sources/Create/View/EnterInfo2View.swift
+++ MeDrop/MeDrop/Sources/Create/View/EnterInfo2View.swift
@@ -19,142 +19,145 @@ struct EnterInfo2View: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                
-                VStack(alignment: .leading) {
-                    Text("한 줄 소개").font(
-                        Font.custom("SF Pro Text", size: 17)
-                        .weight(.bold)
-                        )
-                    VStack {
-                            TextField("예) 성장하는 개발자 김드랍입니다.", text: $editingCard.introduction
-                            ).font(Font.custom("SF Pro Text", size: 17))
-                            
-                        Rectangle()
-                            .frame(height: 1)
-                            
-                    }.foregroundColor(DesignSystemAsset.gray1)
-                }.padding()
-                
-                VStack(alignment: .leading) {
-                    Text("이메일").font(
-                        Font.custom("SF Pro Text", size: 17)
-                        .weight(.bold)
-                        )
-                    VStack {
-                            TextField("medrop@dp.com", text: $editingCard.email
-                            ).font(Font.custom("SF Pro Text", size: 17))
-                            
-                        Rectangle()
-                            .frame(height: 1)
-                            
-                    }.foregroundColor(DesignSystemAsset.gray1)
-                }.padding()
-                
-                VStack(alignment: .leading) {
-                    Text("링크")
-                        .font(
-                        Font.custom("SF Pro Text", size: 17)
-                        .weight(.bold)
-                        )
-                    VStack {
-                        HStack {
-                            if editingCard.insta.isEmpty && editingCard.linkedin.isEmpty && editingCard.youtube.isEmpty && editingCard.github.isEmpty && editingCard.twitter.isEmpty{
-                                Text("더 많은 링크를 추가하세요").font(Font.custom("SF Pro Text", size: 17))
-                                    .foregroundColor(DesignSystemAsset.gray1)
+            ScrollView {
+                VStack {
+                    
+                    VStack(alignment: .leading) {
+                        Text("한 줄 소개").font(
+                            Font.custom("SF Pro Text", size: 17)
+                            .weight(.bold)
+                            )
+                        VStack {
+                                TextField("예) 성장하는 개발자 김드랍입니다.", text: $editingCard.introduction
+                                ).font(Font.custom("SF Pro Text", size: 17))
+                                
+                            Rectangle()
+                                .frame(height: 1)
+                                
+                        }.foregroundColor(DesignSystemAsset.gray1)
+                    }.padding()
+                    
+                    VStack(alignment: .leading) {
+                        Text("이메일").font(
+                            Font.custom("SF Pro Text", size: 17)
+                            .weight(.bold)
+                            )
+                        VStack {
+                                TextField("medrop@dp.com", text: $editingCard.email
+                                ).font(Font.custom("SF Pro Text", size: 17))
+                                
+                            Rectangle()
+                                .frame(height: 1)
+                                
+                        }.foregroundColor(DesignSystemAsset.gray1)
+                    }.padding()
+                    
+                    VStack(alignment: .leading) {
+                        Text("링크")
+                            .font(
+                            Font.custom("SF Pro Text", size: 17)
+                            .weight(.bold)
+                            )
+                        VStack {
+                            HStack {
+                                if editingCard.insta.isEmpty && editingCard.linkedin.isEmpty && editingCard.youtube.isEmpty && editingCard.github.isEmpty && editingCard.twitter.isEmpty{
+                                    Text("더 많은 링크를 추가하세요").font(Font.custom("SF Pro Text", size: 17))
+                                        .foregroundColor(DesignSystemAsset.gray1)
+                                }
+                                else {
+                                    if editingCard.insta.isEmpty == false {
+                                        Image("instagram_").resizable().frame(width: 24, height: 24) }
+                                    if editingCard.twitter.isEmpty == false {
+                                        Image("twitter_").resizable().frame(width: 24, height: 24)
+                                    }
+                                    if editingCard.linkedin.isEmpty == false {
+                                        Image("linkedin_")
+                                    .resizable().frame(width: 24, height: 24)
+                                    }
+                                    if editingCard.youtube.isEmpty == false {
+                                        Image("youtube_").resizable().frame(width: 24, height: 24)
+                                    }
+                                    if editingCard.github.isEmpty == false {
+                                        Image("github_").resizable().frame(width: 24, height: 24)
+                                    }
+                                }
+                                Spacer()
+                                Button(action: { isSNSLink.toggle()}) {
+                                    Image(systemName: "plus.circle").foregroundColor(.black)
+                                }
                             }
-                            else {
-                                if editingCard.insta.isEmpty == false {
-                                    Image("instagram_").resizable().frame(width: 24, height: 24) }
-                                if editingCard.twitter.isEmpty == false {
-                                    Image("twitter_").resizable().frame(width: 24, height: 24)
-                                }
-                                if editingCard.linkedin.isEmpty == false {
-                                    Image("linkedin_")
-                                .resizable().frame(width: 24, height: 24)
-                                }
-                                if editingCard.youtube.isEmpty == false {
-                                    Image("youtube_").resizable().frame(width: 24, height: 24)
-                                }
-                                if editingCard.github.isEmpty == false {
-                                    Image("github_").resizable().frame(width: 24, height: 24)
-                                }
-                            }
-                            Spacer()
-                            Button(action: { isSNSLink.toggle()}) {
-                                Image(systemName: "plus.circle").foregroundColor(.black)
-                            }
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
                         }
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                    }
-                    VStack {
-                        HStack {
-                                Image("link_").resizable().frame(width:24, height: 24)
-                            TextField("블로그, 홈페이지 등의 주소를 입력해주세요.", text: $editingCard.link).foregroundColor(.gray)
-                            Spacer()
-                        }
+                        VStack {
+                            HStack {
+                                    Image("link_").resizable().frame(width:24, height: 24)
+                                TextField("블로그, 홈페이지 등의 주소를 입력해주세요.", text: $editingCard.link).foregroundColor(.gray)
+                                Spacer()
+                            }
+                            
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        }.padding(.top)
                         
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
-                    }.padding(.top)
-                    
-                }.padding()
-                Spacer()
-                HStack {
-                    
-                    Button(action: {
-                        withAnimation(.easeIn(duration: 0.2)){
-                            navigationControl -= 1 }}
-                    ) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10).foregroundColor(Color(red: 0.96, green: 0.95, blue: 0.91))
-                            Text("이전")
-                                .font(Font.custom("SF Pro Text", size: 17))
-                                .foregroundColor(DesignSystemAsset.gray3)
-                        }
-                    }
-                    Button(action: { withAnimation(.easeIn(duration: 0.2)){
-                        navigationControl += 1 }}) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(Color(red: 0.33, green: 0.38, blue: 0.54))
-                                Text("다음")
-                                    .font(Font.custom("SF Pro Text", size: 17))
-                                    .foregroundColor(DesignSystemAsset.white1)
-                            }
-                    }
-                }
-                .frame(height: UIScreen.height * 0.07)
-                .padding()
-            }.navigationTitle("추가 정보를 입력해주세요.")
-            .sheet(isPresented: $isSNSLink) {
-                SNSEnterView(editingCard: $editingCard, isPresented: $isSNSLink)
-            }
-            .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
+                    }.padding()
+                    Spacer()
+                    HStack {
+                        
                         Button(action: {
-                            isBack.toggle()
-                        }) {
-                            Image("x").resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: UIScreen.width*0.04, height: UIScreen.height * 0.04)  
-                            .foregroundColor(.black)
-                            .padding()
+                            withAnimation(.easeIn(duration: 0.2)){
+                                navigationControl -= 1 }}
+                        ) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10).foregroundColor(Color(red: 0.96, green: 0.95, blue: 0.91))
+                                Text("이전")
+                                    .font(Font.custom("SF Pro Text", size: 17))
+                                    .foregroundColor(DesignSystemAsset.gray3)
+                            }
+                        }
+                        Button(action: { withAnimation(.easeIn(duration: 0.2)){
+                            navigationControl += 1 }}) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(Color(red: 0.33, green: 0.38, blue: 0.54))
+                                    Text("다음")
+                                        .font(Font.custom("SF Pro Text", size: 17))
+                                        .foregroundColor(DesignSystemAsset.white1)
+                                }
                         }
                     }
-                }.alert("돌아가시겠어요?", isPresented: $isBack) {
-                    Button("확인", role: .destructive) {
-                        dismiss()
-                    }
-                    Button("취소", role: .cancel) {
-                        isBack.toggle()
-                    }
-                } message: {
-                    Text("지금 돌아간다면 정보는 저장되지 않습니다. ")
+                    .frame(height: UIScreen.height * 0.07)
+                    .padding()
+                }.navigationTitle("추가 정보를 입력해주세요.")
+                .sheet(isPresented: $isSNSLink) {
+                    SNSEnterView(editingCard: $editingCard, isPresented: $isSNSLink)
                 }
+                .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button(action: {
+                                isBack.toggle()
+                            }) {
+                                Image("x").resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: UIScreen.width*0.04, height: UIScreen.height * 0.04)
+                                .foregroundColor(.black)
+                                .padding()
+                            }
+                        }
+            }
+            }
+            .alert("돌아가시겠어요?", isPresented: $isBack) {
+                Button("확인", role: .destructive) {
+                    dismiss()
+                }
+                Button("취소", role: .cancel) {
+                    isBack.toggle()
+                }
+            } message: {
+                Text("지금 돌아간다면 정보는 저장되지 않습니다. ")
+            }
         }
     }
 }
